85f5a235f2bacb07d63af438c14f0011b6cc78c7
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-11-06 06:12:53 +0000

    Deploy bvdwalt/robofriends to github.com/bvdwalt/robofriends.git:gh-pages

---
+++ precache-manifest.1fc0d27bfc48fe4e5af238fc4a282f8a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "586e00e0ce593075fd8a",
+    "url": "/robofriends/static/css/main.fabc1a5a.chunk.css"
+  },
+  {
+    "revision": "586e00e0ce593075fd8a",
+    "url": "/robofriends/static/js/main.586e00e0.chunk.js"
+  },
+  {
+    "revision": "58062d4066549126d817",
+    "url": "/robofriends/static/css/1.0afd2b67.chunk.css"
+  },
+  {
+    "revision": "58062d4066549126d817",
+    "url": "/robofriends/static/js/1.58062d40.chunk.js"
+  },
+  {
+    "revision": "1446d6ec194445c3572d",
+    "url": "/robofriends/static/js/runtime~main.1446d6ec.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "8ec4e99abc81096059d14eb803d625d6",
+    "url": "/robofriends/index.html"
+  }
+];
